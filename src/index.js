3c9a86a9d7cee8be5bc5c56c6d9a23cffedfbd60
MDIMACat <romanmdima@gmail.com>
2024-12-04 19:51:03 +0200

    Working on the hover effect at the moment, trying to make sure it fade when mouse moves out of the grid

--- src/index.js
+++ src/index.js
@@ -1,42 +1,52 @@
 fixedAValues = {
-    classes : {
-        gridContainer: ".container"
-    }
-}
+  classes: {
+    gridContainer: ".container",
+  },
+};
 
 class Skecth {
-    constructor(){
-        this.domElement = {}
-    }
+  constructor() {
+    this.domElement = {};
+  }
 
-    startGame(){
-        this.initializeElements()
-        this.createBody()
-    }
+  startGame() {
+    this.initializeElements();
+    this.createBody();
+  }
 
-    initializeElements(){
-        this.domElement.gridContainer = document.querySelector(
-            fixedAValues.classes.gridContainer
-        )
-    }
+  initializeElements() {
+    this.domElement.gridContainer = document.querySelector(
+      fixedAValues.classes.gridContainer
+    );
+  }
 
-    createBody(){
-        let idx = 1
-        for(let j = 0; j < 16; j++){
-            for(let i = 0; i < 16; i++){
-                const cell = document.createElement("div")
-                cell.classList.add("cell")
-                this.domElement.gridContainer.appendChild(cell)
-                idx++
-            }
-        }
-      
+  createBody() {
+    let idx = 1;
+    for (let j = 0; j < 16; j++) {
+      for (let i = 0; i < 16; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseover", () => {
+            let color = this.generateColor()
+            cell.style.backgroundColor = color
+        });
+        this.domElement.gridContainer.appendChild(cell);
+        idx++;
+      }
     }
+  }
 
-    
+  generateColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
 
-if(typeof document !== undefined){
-    const game = new Skecth()
-    game.startGame()
+if (typeof document !== undefined) {
+  const game = new Skecth();
+  game.startGame();
 }
